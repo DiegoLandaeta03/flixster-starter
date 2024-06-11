1a8aceba34a592e50ab2290b28b3ff6a90b9aa38
Diego Alejandro Landaeta Torres <diegolandaeta@meta.com>
2024-06-10 21:49:21 -0700

    almost have the API working

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -3,6 +3,17 @@ import './MovieList.css';
 import.meta.env.VITE_APP_KEY;
 
 function MovieList(){
+    // let movies = fetchMovies();
+    fetch(import.meta.env.VITE_APP_KEY)
+    .then((response) => {
+        if(response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
     return(
     <main>
         <MovieCard movieTitle='Kingdom of the Plant of the Apes' rating='5' image='https://m.media-amazon.com/images/I/81PqziEQvBL._AC_UF894,1000_QL80_.jpg'/>
@@ -11,5 +22,15 @@ function MovieList(){
     </main>
     );
 }
+
+// function fetchMovies(){
+//     fetch("https://api.themoviedb.org/3/configuration")
+//     .then((response) => {
+//         if(response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+//         return response.json();
+//         })
+// }
 
 export default MovieList;
